9af55602ac297338ffdaa974700a4c1b43fb0bff
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2024-08-27 19:26:31 +0200

    Browsing

--- tests/Controller/TaskControllerTest.php
+++ tests/Controller/TaskControllerTest.php
@@ -2,18 +2,23 @@
 
 namespace App\Tests\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 /**
- * @group smoke
+ * @group functional
  */
 class TaskControllerTest extends WebTestCase
 {
     public function testTaskIndex(): void
     {
-        $this->markTestSkipped();
         $client = static::createClient();
+        $repo = static::getContainer()->get(UserRepository::class);
+        $user = $repo->findOneBy(['username' => 'test']);
+        $client->loginUser($user);
+
         $crawler = $client->request('GET', '/task/');
+        $client->clickLink('Create new');
 
         $this->assertResponseIsSuccessful();
     }
